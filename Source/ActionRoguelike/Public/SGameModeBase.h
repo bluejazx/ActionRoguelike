28d07bc83a43accd434cfebd36dec16334e952ce
bluejazx <josh.jay.724@gmail.com>
2023-10-15 17:34:38 -0400

    Assignment 5 (Credits System, EQS Spawning)
    -Added “Credits-system” to gain and spend
    -Added Killing Minions grants credits via SGameModeBase.
    -Added Health Potion: costs credits to interact
    -Added Coin Pick-up: grants credits
    -Finalized Credits-widget
    -Added Power up Spawn EQS

--- Source/ActionRoguelike/Public/SGameModeBase.h
+++ Source/ActionRoguelike/Public/SGameModeBase.h
@@ -40,13 +40,34 @@ protected:
 	//Variable for time in between spawns
 	float SpawnTimerInterval;
 
+	// Read/write access as we could change this as our difficulty increases via Blueprint
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
+	int32 CreditsPerKill;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Powerups")
+	UEnvQuery* PowerupSpawnQuery;
+
+	/* All power-up classes used to spawn with EQS at match start */
+	UPROPERTY(EditDefaultsOnly, Category = "Powerups")
+	TArray<TSubclassOf<AActor>> PowerupClasses;
+
+	/* Distance required between power-up spawn locations */
+	UPROPERTY(EditDefaultsOnly, Category = "Powerups")
+	float RequiredPowerupDistance;
+
+	/* Amount of powerups to spawn during match start */
+	UPROPERTY(EditDefaultsOnly, Category = "Powerups")
+	int32 DesiredPowerupCount;
+
 	UFUNCTION()
 	//runs after timer
 	void SpawnBotTimerElapsed();
 
 	UFUNCTION()
-	//runs after EQS
-	void OnQueryCompleted(UEnvQueryInstanceBlueprintWrapper* QueryInstance, EEnvQueryStatus::Type QueryStatus);
+	void OnBotSpawnQueryCompleted(UEnvQueryInstanceBlueprintWrapper* QueryInstance, EEnvQueryStatus::Type QueryStatus);
+
+	UFUNCTION()
+	void OnPowerupSpawnQueryCompleted(UEnvQueryInstanceBlueprintWrapper* QueryInstance, EEnvQueryStatus::Type QueryStatus);
 
 	UFUNCTION()
 	void RespawnPlayerElapsed(AController* Controller);
